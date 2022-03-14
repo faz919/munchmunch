e6d5f812fa9ba77a80902f0657abc461ae6eddf8
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 13:05:39 +0300

    logging

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -1,15 +1,15 @@
 const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 
 exports.handler = async (req) => {
+  console.log(req)
   try {
     // const paymentDetails = JSON.parse(req.body)
     const paymentIntent = await stripe.paymentIntents.create({ amount: 1, currency: 'aud' })
+    console.log(JSON.stringify(paymentIntent))
     // Send publishable key and PaymentIntent details to client
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        paymentIntent
-      })
+      body: JSON.stringify({ paymentIntent })
     }
   } catch (e) {
     console.log(e)
