0e8632825617bed2721129ccb601365cc26bbf1e
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 11:04:17 +0300

    yo

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -2,31 +2,30 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 const axios = require('axios')
 
 exports.handler = async (req) => {
-  console.log(req.body)
   axios.post('https://hooks.slack.com/services/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', {
     text: `Request body: ${req.body}`
   })
-  const { paymentDetails } = JSON.parse(req.body)
-  try {
-    const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
-    // Send publishable key and PaymentIntent details to client
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        paymentIntent
-      })
-    }
-  } catch (e) {
-    axios.post('https://hooks.slack.com/services/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', {
-      text: `New error while awaiting third-party-pay fetch: ${e.message}`
-    })
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        error: {
-          message: e.message
-        }
-      })
-    }
-  }
+  // const { paymentDetails } = JSON.parse(req.body)
+  // try {
+  //   const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
+  //   // Send publishable key and PaymentIntent details to client
+  //   return {
+  //     statusCode: 200,
+  //     body: JSON.stringify({
+  //       paymentIntent
+  //     })
+  //   }
+  // } catch (e) {
+  //   axios.post('https://hooks.slack.com/services/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', {
+  //     text: `New error while awaiting third-party-pay fetch: ${e.message}`
+  //   })
+  //   return {
+  //     statusCode: 400,
+  //     body: JSON.stringify({
+  //       error: {
+  //         message: e.message
+  //       }
+  //     })
+  //   }
+  // }
 }
