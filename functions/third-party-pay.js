a7d3bd511aa367a4c7a9688ce9db0395b321272e
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 10:53:25 +0300

    moving backwards

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -2,12 +2,12 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 const axios = require('axios')
 
 exports.handler = async (req) => {
-  console.log(req.body)
+  console.log(req?.body)
   axios.post('https://hooks.slack.com/services/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', {
-    text: `Request body: ${req.body}`
+    text: `Request body: ${req?.body}`
   })
   try {
-    const { paymentDetails } = JSON.parse(req.body)
+    const { paymentDetails } = JSON.parse(req?.body)
     const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
     // Send publishable key and PaymentIntent details to client
     return {
