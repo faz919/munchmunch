8b76fe27d8eb3dc0c6684a88abcb9b2b7f92c70f
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 11:55:19 +0300

    maybe

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -2,34 +2,30 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 const axios = require('axios')
 
 exports.handler = async (req) => {
-  if (req.body) {
-    try {
-      const paymentDetails = JSON.parse(req.body)
-      const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
-      // Send publishable key and PaymentIntent details to client
-      return {
-        statusCode: 200,
-        body: JSON.stringify({
-          paymentIntent
-        })
-      }
-    } catch (e) {
-      axios.post('https://hooks.slack.com/services/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', {
-        text: `New error while awaiting third-party-pay fetch: ${e.message}`
-      })
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          error: {
-            message: e.message
-          }
-        })
-      }
-    }
-  } else {
+  try {
+    const paymentDetails = JSON.parse(req.body)
+    axios.post('https://hooks.slack.com/services/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', {
+      text: `New req body: ${req.body}`
+    })
+    const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
+    // Send publishable key and PaymentIntent details to client
     return {
       statusCode: 200,
-      body: 'No payment request params found.'
+      body: JSON.stringify({
+        paymentIntent
+      })
+    }
+  } catch (e) {
+    axios.post('https://hooks.slack.com/services/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', {
+      text: `New error while awaiting third-party-pay fetch: ${e.message}`
+    })
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        error: {
+          message: e.message
+        }
+      })
     }
   }
 }
