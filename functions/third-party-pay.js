2f54e66f4dc3d25b95d20c3176f7f20f4781f357
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 10:43:16 +0300

    please work

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -3,8 +3,8 @@ const axios = require('axios')
 
 exports.handler = async (req) => {
   try {
-    const { amount, currency, paymentDetails } = JSON.parse(req.body)
-    const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails, amount, currency })
+    const { paymentDetails } = JSON.parse(req.body)
+    const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
     // Send publishable key and PaymentIntent details to client
     return {
       statusCode: 200,
@@ -14,7 +14,7 @@ exports.handler = async (req) => {
     }
   } catch (e) {
     axios.post('https://hooks.slack.com/services/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', {
-      text: `New error while awaiting third-party-pay fetch: ${JSON.stringify(e)}`
+      text: `New error while awaiting third-party-pay fetch: ${e.message}`
     })
     return {
       statusCode: 400,
