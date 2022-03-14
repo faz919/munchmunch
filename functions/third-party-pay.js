a11ec76e777c0807b5aeba7fdc6584ac1be1fb8e
Allen Sabet <allensabet@Allens-MacBook-Pro.local>
2022-03-14 10:48:03 +0300

    yo

--- functions/third-party-pay.js
+++ functions/third-party-pay.js
@@ -2,6 +2,10 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 const axios = require('axios')
 
 exports.handler = async (req) => {
+  console.log(req.body)
+  axios.post('https://hooks.slack.com/services/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', {
+    text: `Request body: ${req.body}`
+  })
   try {
     const { paymentDetails } = JSON.parse(req.body)
     const paymentIntent = await stripe.paymentIntents.create({ ...paymentDetails })
