552a281c58fe5308dcb05506f88fd7a7630c25d8
Dmytro Petrenko <petrenko.d28@gmail.com>
2022-04-13 21:42:33 +0300

    replace element to app root

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import './index.scss';
 import { Routes, Route } from 'react-router-dom';
-// Stripe
-import { Elements } from '@stripe/react-stripe-js';
-import { loadStripe } from '@stripe/stripe-js';
+
 import Home from './pages/Home';
 import PetName from './pages/PetName';
 import WeightAndAge from './pages/WeightAndAge';
 import TargetWeight from './pages/TargetWeight';
 import Checkout from './pages/Checkout';
 
-const stripePromise = loadStripe(
-  `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
-);
-
 function App() {
   return (
-    <Elements stripe={stripePromise}>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='dog-name' element={<PetName />} />
@@ -24,7 +17,6 @@ function App() {
         <Route path='dog-weight-required' element={<TargetWeight />} />
         <Route path='checkout' element={<Checkout />} />
       </Routes>
-    </Elements>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import { AppStateProvider } from './context';
 import { theme } from './theme';
+// Stripe
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+
+const stripePromise = loadStripe(
+  `${process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY}`
+);
 
 ReactDOM.render(
   <>
@@ -13,7 +20,9 @@ ReactDOM.render(
     <AppStateProvider>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </ThemeProvider>
       </BrowserRouter>
     </AppStateProvider>
