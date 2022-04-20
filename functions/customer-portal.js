d39cd3cbd240ba828b7d02a45db1936d3d9695e2
faz919 <faizit9@gmail.com>
2022-04-20 17:13:20 +0300

    email template test

--- functions/customer-portal.js
+++ functions/customer-portal.js
@@ -37,7 +37,7 @@ exports.handler = async (req) => {
         to: email,
         subject: 'Welcome!',
         template: "welcome",
-        'h:X-Mailgun-Variables': {'%recipient.portal%': portalSession.url}
+        '%recipient.portal%': JSON.stringify(portalSession.url)
       }
       try {
         await client.messages.create(DOMAIN, messageData)
