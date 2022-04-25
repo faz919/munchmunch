11fa4d8f242dd00210dd70fe672551aa08db9d04
faz919 <faizit9@gmail.com>
2022-04-25 08:04:29 +0300

    bit of error logging

--- functions/calculate-price.js
+++ functions/calculate-price.js
@@ -3,37 +3,35 @@ const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 exports.handler = async (req) => {
   const { meatTypes, kgsPerMeatType } = JSON.parse(req.body)
     const items = meatTypes.map(async (meat) => {
+      let price, quantity
       switch (meat) {
         case 'beef':
-            const beef_price = await stripe.prices.retrieve(`${process.env.PRICE_ID_BEEF}`)
-            const beef_quantity = kgsPerMeatType['beef'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
-          return beef_price['unit_amount'] * beef_quantity
+            price = await stripe.prices.retrieve(`${process.env.PRICE_ID_BEEF}`)
+            quantity = kgsPerMeatType['beef'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
+            break
         case 'chicken':
-            const chicken_price = await stripe.prices.retrieve(`${process.env.PRICE_ID_CHICKEN}`)
-            const chicken_quantity = kgsPerMeatType.chicken / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
-          return kgsPerMeatType.chicken
+            price = await stripe.prices.retrieve(`${process.env.PRICE_ID_CHICKEN}`)
+            quantity = kgsPerMeatType.chicken / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
+            break
         case 'lamb':
-            const lamb_price = await stripe.prices.retrieve(`${process.env.PRICE_ID_LAMB}`)
-            const lamb_quantity = kgsPerMeatType.lamb / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
-          return kgsPerMeatType.lamb
+            price = await stripe.prices.retrieve(`${process.env.PRICE_ID_LAMB}`)
+            quantity = kgsPerMeatType.lamb / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
+            break
         case 'turkey':
-            const turkey_price = await stripe.prices.retrieve(`${process.env.PRICE_ID_TURKEY}`)
-            const turkey_quantity = kgsPerMeatType['turkey'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
-          return turkey_price['unit_amount'] * turkey_quantity
+            price = await stripe.prices.retrieve(`${process.env.PRICE_ID_TURKEY}`)
+            quantity = kgsPerMeatType['turkey'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
+            break
         case 'kangaroo':
-            const kangaroo_price = await stripe.prices.retrieve(`${process.env.PRICE_ID_KANGAROO}`)
-            const kangaroo_quantity = kgsPerMeatType['kangaroo'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
-          return kangaroo_price['unit_amount'] * kangaroo_quantity
-        default:
-            return 'meat type not found'
+            price = await stripe.prices.retrieve(`${process.env.PRICE_ID_KANGAROO}`)
+            quantity = kgsPerMeatType['kangaroo'] / (parseInt(process.env.GRAMS_PER_UNIT) / 1000)
+            break
       }
+      return price['unit_amount'] * quantity
     })
     const total_unit_amount = items.reduce((partialSum, a) => partialSum + a, 0);
     return {
       statusCode: 200,
       body: JSON.stringify({
-        something: kgsPerMeatType.chicken, 
-        other_thing: kgsPerMeatType['chicken'],
         items,
         subtotal: total_unit_amount/100
       })
