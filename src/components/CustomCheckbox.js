41a853a953e5585f8e11040e40894c191bf01913
faz919 <faizit9@gmail.com>
2022-04-26 17:33:18 +0300

    custom checkbox

--- src/components/CustomCheckbox.js
+++ src/components/CustomCheckbox.js
@@ -6,32 +6,27 @@ const BpIcon = styled('span')(({ theme }) => ({
     borderRadius: 3,
     width: '40px',
     height: '40px',
-    boxShadow:
-        theme.palette.mode === 'dark'
-            ? '0 0 0 1px rgb(16 22 26 / 40%)'
-            : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
-    backgroundColor: theme.palette.mode === 'dark' ? '#394b59' : '#f5f8fa',
-    backgroundImage:
-        theme.palette.mode === 'dark'
-            ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
-            : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
+    // boxShadow:
+    //     theme.palette.mode === 'dark'
+    //         ? '0 0 0 1px rgb(16 22 26 / 40%)'
+    //         : 'inset 0 0 0 1px rgba(16,22,26,.2), inset 0 -1px 0 rgba(16,22,26,.1)',
+    backgroundColor: '#fff',
+    // backgroundImage:
+    //     theme.palette.mode === 'dark'
+    //         ? 'linear-gradient(180deg,hsla(0,0%,100%,.05),hsla(0,0%,100%,0))'
+    //         : 'linear-gradient(180deg,hsla(0,0%,100%,.8),hsla(0,0%,100%,0))',
     '.Mui-focusVisible &': {
         outline: '2px auto rgba(254,101,79,.6)',
         outlineOffset: 2,
     },
     'input:hover ~ &': {
-        backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
-    },
-    'input:disabled ~ &': {
-        boxShadow: 'none',
-        background:
-            theme.palette.mode === 'dark' ? 'rgba(57,75,89,.5)' : 'rgba(206,217,224,.5)',
+        backgroundColor: '#fff',
     },
 }));
 
 const BpCheckedIcon = styled(BpIcon)({
     backgroundColor: 'rgba(254,101,79,.7)',
-    backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
+    // backgroundImage: 'linear-gradient(180deg,hsla(0,0%,100%,.1),hsla(0,0%,100%,0))',
     '&:before': {
         display: 'block',
         width: '40px',
