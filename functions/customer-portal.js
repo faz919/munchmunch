585cf76c2807e972359d1622fde91692a34a771c
faz919 <faizit9@gmail.com>
2022-04-26 16:42:07 +0300

    other email template

--- functions/customer-portal.js
+++ functions/customer-portal.js
@@ -33,7 +33,7 @@ exports.handler = async (req) => {
         return_url: return_url
       })
       const messageData = {
-        from: 'MunchMunch Support <support@munchmunch.com.au>',
+        from: 'Munch+Munch <support@munchmunch.com.au>',
         to: email,
         subject: 'Welcome!',
         template: "welcome",
