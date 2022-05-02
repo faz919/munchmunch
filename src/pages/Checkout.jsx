57592c0b3daa65f8dfe66347e390b3a846b06087
Sanjay Hashcrypt <90251924+sanjayhashcrypt@users.noreply.github.com>
2022-05-02 11:20:22 +0530

    Update Checkout.jsx

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -32,8 +32,8 @@ const StyledHeading1 = styled.p`
     background-image: -webkit-linear-gradient(0deg, rgb(255, 204, 51) 2%, rgb(226, 51, 255) 100%);
     background-image: -moz-linear-gradient(0deg, rgb(255, 204, 51) 2%, rgb(226, 51, 255) 100%);
     text-shadow: none;
-    padding: 15px 25px;
-    font-size: 21px;
+    padding: 15px 18px;
+    font-size: 15px;
     border-radius: 33px;
 `;
 
