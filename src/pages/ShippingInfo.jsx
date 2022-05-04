48bd52ca2158d6a51d1eff538ceab4e6dae99309
Raj Radhanpura <hashcrypthash@gmail.com>
2022-05-04 14:45:07 +0530

    Update ShippingInfo.jsx

--- src/pages/ShippingInfo.jsx
+++ src/pages/ShippingInfo.jsx
@@ -75,7 +75,7 @@ const ShippingInfo = () => {
                             callback={(e) =>
                                 setClientInfo((val) => ({ ...val, name: e.target.value }))
                             }
-                        />
+                        autoFocus/>
                         <FormInputElement
                             type='email'
                             label='Email'
